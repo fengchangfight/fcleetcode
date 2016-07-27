d2f776f87a662c4108d7a24f7666ea174fbefd32
fengchang <fengchang.xie@cloudchef.io>
2016-07-27 16:56:05 +0800

    incomplete working version

--- 2_Add_Two_Numbers/Add_Two_Numbers.py
+++ 2_Add_Two_Numbers/Add_Two_Numbers.py
@@ -13,18 +13,45 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        stack1=[]
-        stack2=[]
+
         result = None
+        tail = None
         h1 = l1
         h2 = l2
-        while(h1!=None):
-            stack1.append(h1.val)
-            h1=h1.next
-        while (h2 != None):
-            stack1.append(h2.val)
-            h2 = h2.next
         overflowflag = False
-        while(len(stack1)>0 and len(stack2)>0):
-            tmpadd = stack1.pop()+stack2.pop()
+        overflowvalue = 0
+        while(h1!=None and h2!=None):
+            tmpadd = h1.val+h2.val+overflowvalue
+            if(tmpadd >= 10):
+                overflowflag = True
+                overflowvalue = tmpadd/10
+                tmpadd = tmpadd % 10
+            else:
+                overflowflag = False
+                overflowvalue = 0
+            if(result == None):
+                result = ListNode(tmpadd)
+                tail = result
+            else:
+                tail.next = ListNode(tmpadd)
+                tail = tail.next
+            h1 = h1.next
+            h2 = h2.next
+        if(overflowflag):
+            tail.next = ListNode(overflowvalue)
+        return result
 
+sol = Solution()
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+result = sol.addTwoNumbers(l1,l2)
+head = result
+while(head!=None):
+    print (head.val)
+    head = head.next

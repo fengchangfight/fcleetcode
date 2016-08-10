2117e02634cc6e96b3dd3070c3e7e78f2162dd83
fengchang <fengchang.xie@cloudchef.io>
2016-08-10 20:24:36 +0800

    second TLE version

--- 5_Longest_Palindromic_Substring/Longest_Palindromic_Substring.py
+++ 5_Longest_Palindromic_Substring/Longest_Palindromic_Substring.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python
 
 class Solution(object):
-    def palindromeatpoint(self, s, index):
-        if(s==None or len(s)==0 or index<0 or index>=len(s)):
-            return None
-        i=j=1
-        oddsize = 1
-        while(index-i>=0 and index+i<len(s)):
-            if(s[index-i]==s[index+i]):
-                oddsize+=2
-                i += 1
+    def isPalindrome(self,s,start,end):
+        while(end>=start):
+            if(s[start]==s[end]):
+                start += 1
+                end -= 1
+                continue
             else:
-                break
-        if(index<len(s)-1 and s[index]==s[index+1]):
-            evensize = 2
-            while (index - j >= 0 and index + 1 + j < len(s)):
-                if (s[index - j] == s[index + 1 + j]):
-                    evensize += 2
-                    j += 1
-                else:
-                    break
-        else:
-            evensize = -1
-        if(evensize > oddsize):
-            start = index - j + 1
-            return s[start:start+evensize]
-        else:
-            start = index - i + 1
-            return s[start:start+oddsize]
+                return False
+        return True
 
+    def containsPalindromeOfLengthK(self,s,k):
+        if(k>=1 and k<=len(s)):
+            for i in range(0,len(s)-k+1) :
+                if(self.isPalindrome(s,i,i+k-1)):
+                    return s[i:i+k]
+            return False
+        else:
+            return False
     def longestPalindrome(self, s):
         """
         :type s: str
@@ -37,19 +27,13 @@ class Solution(object):
         #sample :fadsgsdyi
         maxlen = -1
         for i in range(0, len(s)):
-            sub = self.palindromeatpoint(s,i)
-            if(len(sub)>maxlen):
-                maxlen=len(sub)
-                result = sub
-        return result
-
-
+            result = self.containsPalindromeOfLengthK(s,len(s)-i)
+            if(result):
+                return result
+        return None
 
 str = 'aadsggsdyi'
 sol = Solution()
-
-
-
 
 print(sol.longestPalindrome(str))
 

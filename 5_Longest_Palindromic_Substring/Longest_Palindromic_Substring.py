69c19e8d51e54a0791bb7533fd079ae88100cbe1
fengchang <fengchang.xie@cloudchef.io>
2016-08-10 20:57:40 +0800

    first working version

--- 5_Longest_Palindromic_Substring/Longest_Palindromic_Substring.py
+++ 5_Longest_Palindromic_Substring/Longest_Palindromic_Substring.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python
 
 class Solution(object):
-    def isPalindrome(self,s,start,end):
-        while(end>=start):
-            if(s[start]==s[end]):
-                start += 1
-                end -= 1
-                continue
-            else:
-                return False
-        return True
-
-    def containsPalindromeOfLengthK(self,s,k):
-        if(k>=1 and k<=len(s)):
-            for i in range(0,len(s)-k+1) :
-                if(self.isPalindrome(s,i,i+k-1)):
-                    return s[i:i+k]
-            return False
-        else:
-            return False
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
         #sample :fadsgsdyi
-        maxlen = -1
-        for i in range(0, len(s)):
-            result = self.containsPalindromeOfLengthK(s,len(s)-i)
-            if(result):
-                return result
-        return None
+     #   maxlen = -1
+        if s==None or len(s)==0:
+            return ""
+        maxLen = 0
+        res = ""
+        for i in range(0,2*len(s)-1):
+            left = i/2
+            right = i/2
+            if(i%2==1):
+                right +=1
+            str = self.lengthOfPalindrome(s, left,right)
+            if(maxLen<len(str)):
+                maxLen = len(str)
+                res = str
+        return res
+
+    def lengthOfPalindrome(self,s,left, right):
+        while left>=0 and right<len(s) and s[left]==s[right]:
+            left -= 1
+            right += 1
+        return s[left+1:right]
 
 str = 'aadsggsdyi'
 sol = Solution()
